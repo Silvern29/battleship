0dcc5020e9fcc8d70c9c09d90c8910c812a9b119
Florian Redlinghaus <florian@redlinghaus.at>
2020-01-21 23:43:35 +0100

    up

---
+++ bstypescript/tuple.js
@@ -0,0 +1,8 @@
+"use strict";
+var organizedArray = [0, 'text', false];
+var myArray;
+myArray = [0, 'text', false];
+console.log(myArray);
+var myTuple;
+myTuple = [0, 'text', false];
+console.log(myTuple);
