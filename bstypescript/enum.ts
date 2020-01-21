158fbbe3061dc3d78b386622ae303e88cd237fd0
Silvern29 <florian@redlinghaus.at>
2020-01-21 15:35:50 +0100

    up

---
+++ bstypescript/enum.ts
@@ -0,0 +1,9 @@
+enum bands{
+    Motorhead,
+    Metallica,
+    Slayer
+}
+console.log(bands);
+
+let myFavoriteBand = bands[2];
+console.log(myFavoriteBand);
---
+++ bstypescript/tuple.ts
@@ -0,0 +1,9 @@
+const organizedArray: [number, string, boolean] = [0, 'text', false];
+let myArray: [number, string, boolean];
+myArray = [0, 'text', false];
+console.log(myArray);
+
+type Tuple = [number, string, boolean];
+let myTuple: Tuple;
+myTuple = [0, 'text', false];
+console.log(myTuple);
