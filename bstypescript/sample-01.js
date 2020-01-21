3fcd53e5264a2fff67972e91ec9e2ce1ddd1e1bd
Silvern29 <florian@redlinghaus.at>
2020-01-21 15:11:07 +0100

    up

---
+++ bstypescript/sample-01.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log('First Sample With TypeScript');
+var MyClass = /** @class */ (function () {
+    function MyClass() {
+    }
+    MyClass.sum = function (x, y) {
+        console.log('Number is: ', x + y);
+        return x + y;
+    };
+    return MyClass;
+}());
+MyClass.sum(3, 5);
